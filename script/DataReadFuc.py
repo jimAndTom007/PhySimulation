ca7fc2d7dd11fdd37f9f469f8323afeeb06db475
amazingZ <zhangjingcc524@163.com>
2021-02-22 19:59:00 +0800

    ENH：增加PSS/SSS序列函数

--- script/DataReadFuc.py
+++ script/DataReadFuc.py
@@ -11,8 +11,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import copy
 import scipy.io as sio
+import scipy.fftpack as scf
 import os
 import re
+
+
+def pss_sequence(n_id2):
+    x = [1, 1, 1, 0, 1, 1, 0]
+    x_seq = np.zeros([127])
+    for n in range(127):
+        x_seq[n] = x[n] if n <= 6 else (x_seq[n - 7 + 4] + x_seq[n - 7]) % 2
+
+    sequence = [1 - 2 * x_seq[(m + 43 * n_id2) % 127] for m in range(127)]
+    return sequence
+
+
+def sss_sequence(n_id1, n_id2):
+    pass
 
 
 def reading(path_folder, file):
@@ -23,29 +38,40 @@ def reading(path_folder, file):
     return d_array
 
 
-def readmat(path_folder, file):
-    filepath = path.join(path_folder, file)
+def main():
+    path_folder = r'D:\document\5g_nr_data_122M88\Matlab'
+    files = [r'5g_nr_subframe_10.mat']
+    filepath = path.join(path_folder, files[0])
     mat_tmp = sio.loadmat(filepath)
+    timeData = mat_tmp['waveStruct']['waveform'][0, 0]
+    cpStartSet = [352 + (4096 + 288) * i for i in range(14)]
+    for symidx in [0]:
+        for cpStart in cpStartSet:
+            symStart = 61440 * symidx + cpStart
+            symData = timeData[symStart:symStart + 4096, 0]
+            freData = scf.fftshift(scf.fft(symData))
+            plt.figure()
+            plt.plot(abs(freData))
+
+    plt.show()
 
 
 if __name__ == '__main__':
-    path_floder = r'D:\document\5g_nr_data_122M88\仪器厂商'
-    files = [r'5g_nr_rs_fdd_i.dat',
-             r'5g_nr_rs_fdd_q.dat']
-
-    d_i = reading(path_floder, files[0])
-    d_q = reading(path_floder, files[1])
-    d = d_i + d_q * 1j
-    d_abs=abs(d)
-    d_bool=np.bool_(d.shape)
-    d_plt= copy.copy(d[:500000])
-    plt.figure()
-    plt.scatter(np.arange(d.size),abs(d))
+    # path_floder = r'D:\document\5g_nr_data_122M88\仪器厂商'
+    # files = [r'5g_nr_rs_fdd_i.dat',
+    #          r'5g_nr_rs_fdd_q.dat']
+    # pointnum=61440*4
+    # d_i = reading(path_floder, files[0])
+    # d_q = reading(path_floder, files[1])
+    # d = d_i + d_q * 1j
+    # d_abs=abs(d)
+    # d_bool=np.bool_(d.shape)
+    # d_plt= copy.copy(d[:pointnum])
+    # # plt.figure()
+    # # plt.scatter(np.arange(d.size),abs(d))
     # plt.figure()
-    # plt.scatter(np.arange(500000),abs(d_plt))
-    plt.show()
+    # plt.scatter(np.arange(pointnum),abs(d_plt))
+    # plt.show()
 
     #################mat################################
-    # path_folder = r'D:\document\5g_nr_data_122M88\Matlab'
-    # files = [r'5g_nr_subframe_10.mat']
-    # readmat(path_folder, files[0])
+    pss_sequence(0)
